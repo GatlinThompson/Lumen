89bf73d69a626881bbe5166347dc7d72730b1cdc
atleyegbert <atleyegbert@mail.weber.edu>
2025-04-03 11:11:19 -0600

    trainer, manager, and employee dashboards created

--- client/src/components/admin-dashboard/ManagerDashboard.jsx
+++ client/src/components/admin-dashboard/ManagerDashboard.jsx
@@ -4,25 +4,16 @@ import TrainingPrograms from "./TrainingPrograms";
 import TrainingInsights from "./TrainingInsights";
 import TrainingDeadlines from "./TrainingDeadlines";
 
-export default function AdminDashboard() {
+export default function ManagerDashboard() {
   return (
     <>
       <div className="row">
         <div className="col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-5 mt-4">
           <div>
-            <UsersOverview />
-          </div>
-          <div>
-            <IconButtons />
-          </div>
-          <div>
             <TrainingInsights />
           </div>
         </div>
         <div className="col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-5 mt-4">
-          <div>
-            <TrainingPrograms />
-          </div>
           <div>
             <TrainingDeadlines />
           </div>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,9 @@ import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../App.jsx";
 import { useNavigate } from "react-router-dom";
 import AdminDashboard from "../components/admin-dashboard/AdminDashBoard.jsx";
+import ManagerDashboard from "../components/admin-dashboard/ManagerDashboard.jsx";
+import TrainerDashboard from "../components/admin-dashboard/TrainerDashBoard.jsx";
+import EmployeeDashboard from "../components/admin-dashboard/EmployeeDashboard.jsx";
 import PageHeader from "../components/basic-components/PageHeader.jsx";
 
 export default function Dashboard() {
@@ -21,6 +24,9 @@ export default function Dashboard() {
         <div className="pt-4">
           <PageHeader title={`Welcome back, ${user.first_name}`} />
           {user && user.role == "admin" && <AdminDashboard />}
+          {user && user.role == "manager" && <ManagerDashboard />}
+          {user && user.role == "trainer" && <TrainerDashboard />}
+          {user && user.role == "employee" && <EmployeeDashboard />}
         </div>
       ) : null}
     </div>
