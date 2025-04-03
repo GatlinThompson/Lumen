15d230e4494f14939213d8da1a2daf9c19c869f2
atleyegbert <atleyegbert@mail.weber.edu>
2025-04-02 20:17:26 -0600

    test dash

--- client/src/components/admin-dashboard/AdminDashBoard.jsx
+++ client/src/components/admin-dashboard/AdminDashBoard.jsx
@@ -8,7 +8,7 @@ export default function AdminDashboard() {
   return (
     <>
       <div className="row">
-        <div className="col-12 col-md-6 mt-4">
+        <div className="col-12 col-md-6 col-xl-4 mt-4">
           <div>
             <UsersOverview />
           </div>
@@ -19,7 +19,7 @@ export default function AdminDashboard() {
             <TrainingInsights />
           </div>
         </div>
-        <div className="col-12 col-md-6 mt-4">
+        <div className="col-12 col-md-6 col-xl-4 mt-4">
           <div>
             <TrainingPrograms />
           </div>
